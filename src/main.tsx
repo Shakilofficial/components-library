19651bb589ca13bb9c58a03b1f62c67ae018cc16
ShakilOfficial <mrshakilhossainofficial@gmail.com>
2025-01-16 14:04:29 +0600

    setup redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import { Toaster } from "./components/ui/toaster.tsx";
 import "./index.css";
+import { store } from "./redux/store.ts";
 import router from "./routes/Routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Toaster />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+        <Toaster />
+      </ThemeProvider>
+    </Provider>
   </StrictMode>
 );
